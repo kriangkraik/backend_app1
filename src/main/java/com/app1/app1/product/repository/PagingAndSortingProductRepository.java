377af86aaf2ce94168bb09b19346811ea1c33c7f
Kriangkrai Ketkun <kriangkraiketkun@gmail.com>
2025-07-05 08:13:26 +0700

    feat: Add Mockito and ByteBuddy versions to pom.xml; refactor ProductRepository to extend PagingAndSortingProductRepository

---
+++ src/main/java/com/app1/app1/product/repository/PagingAndSortingProductRepository.java
@@ -0,0 +1,8 @@
+package com.app1.app1.product.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface PagingAndSortingProductRepository<T1, I> extends JpaRepository<T1, I> {
+}
--- src/main/java/com/app1/app1/product/repository/ProductRepository.java
+++ src/main/java/com/app1/app1/product/repository/ProductRepository.java
@@ -8,12 +8,13 @@ import java.math.BigDecimal;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+@Repository
+public interface ProductRepository extends PagingAndSortingProductRepository<Product, Long> {
 
     // ตรวจสอบว่ามีรหัสสินค้านี้อยู่แล้วหรือไม่
     boolean existsByCode(String code);
